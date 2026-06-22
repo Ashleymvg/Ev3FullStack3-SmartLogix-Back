4b36fb390bdf63183761ac6d095123cd4aa63f17
Ashley Vargas <ashl.vargas@duocuc.cl>
2026-06-22 17:14:00 -0400

    Creacion de nuevos productos

--- inventory-service/src/main/java/com/smartlogix/inventory/config/InventorySeedConfig.java
+++ inventory-service/src/main/java/com/smartlogix/inventory/config/InventorySeedConfig.java
@@ -19,6 +19,13 @@ public class InventorySeedConfig {
             repository.save(buildItem("SKU-1001", "Teclado Mecanico RGB", "WH-SCL-01", 120, 20));
             repository.save(buildItem("SKU-2001", "Mouse Inalambrico", "WH-SCL-01", 200, 30));
             repository.save(buildItem("SKU-3001", "Monitor 24 Pulgadas", "WH-VAP-02", 45, 10));
+
+            repository.save(buildItem("SKU-4001", "Gabinete Gamer RGB", "WH-SCL-01", 30, 5));
+            repository.save(buildItem("SKU-5001", "Pasta Termica 3g", "WH-VAP-02", 150, 20));
+            repository.save(buildItem("SKU-6001", "Disco Duro SSD 1TB", "WH-SCL-01", 80, 15));
+            repository.save(buildItem("SKU-7001", "Memoria RAM 16G DDR4", "WH-VAP-02", 100, 20));
+            repository.save(buildItem("SKU-8001", "Fuente de Poder 650W 80+ Bronze", "WH-SCL-01", 40, 10));
+            repository.save(buildItem("SKU-9001", "Disco Duro SSD 1TB", "WH-VAP-02", 65, 12));
         };
     }
 
